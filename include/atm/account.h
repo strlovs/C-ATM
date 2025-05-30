757f6a030b0aeaf976ecc2fb92c1e4af1314285c
Laylla Figueiredo <laylla.lima@esoftware.uniceplac.edu.br>
2025-05-30 09:54:50 -0300

    Update account.h

---
+++ include/atm/account.h
@@ -0,0 +1,10 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include "storage.h"
+
+// Exibe as informações da conta autenticada
+void exibirInformacoesConta(const Conta* conta);
+
+#endif
+// laylla figueiredo
